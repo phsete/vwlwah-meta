8449d690cdbd097f2da2eee5cde6893111e053e1
Florian Grieskamp <f.grieskamp@gmail.com>
2019-04-25 08:54:41 +0200

    Update Plots

--- memoryplots/plot.py
+++ memoryplots/plot.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 import math
 
 # evenly sampled time at 200ms intervals
-d = np.array([1/(1+math.exp(-i)) for i in np.arange(-40, 40, 0.001)])
+# d = np.array([1/(1+math.exp(-i)) for i in np.arange(-25, 25, 0.001)])
+d = np.array([1/(1+math.exp(-i)) for i in np.arange(-25, 0, 0.001)])
 f = 8
 N = 100000000
 w = 32
@@ -43,10 +44,10 @@ plt.plot(d, (
 # Log Append
 lessList = [k for k in range(1,math.ceil((math.log((math.floor(N/(w-1))-1),2))/(w-2)) - 1)]
 lessEqualList = [k for k in range(1,math.floor((math.log((math.floor(N/(w-1))-1),2))/(w-2)))]
-print(3, sep="\n")
-print(w / N * (math.floor(N / (w-1) - 1)) * (PL + PN0F * P0F + PN1F * P1F))
-print(w / N * sum([(P0F**(1+2**(k*(w-2))) + P1F**(1+2**(k*(w-2)))) for k in lessEqualList]), sep="\n")
-print(w / N * sum([(math.floor(N/(w-1)) - 1 - 2**(k*(w-2))) * (PN0F * P0F**(1+2**(k*(w-2))) + PN1F * P1F**(1+2**(k*(w-2)))) for k in lessList]), sep="\n")
+# print(3, sep="\n")
+# print(w / N * (math.floor(N / (w-1) - 1)) * (PL + PN0F * P0F + PN1F * P1F))
+# print(w / N * sum([(P0F**(1+2**(k*(w-2))) + P1F**(1+2**(k*(w-2)))) for k in lessEqualList]), sep="\n")
+# print(w / N * sum([(math.floor(N/(w-1)) - 1 - 2**(k*(w-2))) * (PN0F * P0F**(1+2**(k*(w-2))) + PN1F * P1F**(1+2**(k*(w-2)))) for k in lessList]), sep="\n")
 
 plt.plot(d, (
     3
@@ -57,7 +58,7 @@ plt.plot(d, (
         for k in lessList])
     ) * w / N, 'g--')
 
-w=3
+w=22
 P1F = d**(w-1)
 P0F = (1-d)**(w-1)
 PN1F = 1 - P1F
@@ -66,10 +67,10 @@ PL  = 1 - P0F - P1F
 # Log Append
 lessList = [k for k in range(1,math.ceil((math.log((math.floor(N/(w-1))-1),2))/(w-2)) - 1)]
 lessEqualList = [k for k in range(1,math.floor((math.log((math.floor(N/(w-1))-1),2))/(w-2)))]
-print(4, sep="\n")
-print((math.floor(N / (w-1) - 1)) * (PL + PN0F * P0F + PN1F * P1F) * w / N)
-print(w / N * sum([(P0F**(1+2**(k*(w-2))) + P1F**(1+2**(k*(w-2)))) for k in lessEqualList]), sep="\n")
-print(w / N * sum([(math.floor(N/(w-1)) - 1 - 2**(k*(w-2))) * (PN0F * P0F**(1+2**(k*(w-2))) + PN1F * P1F**(1+2**(k*(w-2)))) for k in lessList]), sep="\n")
+# print(4, sep="\n")
+# print((math.floor(N / (w-1) - 1)) * (PL + PN0F * P0F + PN1F * P1F) * w / N)
+# print(w / N * sum([(P0F**(1+2**(k*(w-2))) + P1F**(1+2**(k*(w-2)))) for k in lessEqualList]), sep="\n")
+# print(w / N * sum([(math.floor(N/(w-1)) - 1 - 2**(k*(w-2))) * (PN0F * P0F**(1+2**(k*(w-2))) + PN1F * P1F**(1+2**(k*(w-2)))) for k in lessList]), sep="\n")
 
 plt.plot(d, (
     3
@@ -85,5 +86,6 @@ plt.plot(d, (
 # plt.plot(d, v * d / (v-1) * (1 + (2*v - 3) / f), 'g--')
 
 plt.plot(d, 1+d-d, 'r--')
-plt.xscale('logit')
+plt.xscale('log')
+# plt.xscale('logit')
 plt.show()
