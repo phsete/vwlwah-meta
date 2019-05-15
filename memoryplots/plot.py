54faf56f97d30e084c7018ec7ab7fc3b208df103
Florian Grieskamp <f.grieskamp@gmail.com>
2019-05-15 15:57:32 +0200

    Modify python script to generate LaTeX Plots

--- memoryplots/plot.py
+++ memoryplots/plot.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 import math
 
 # evenly sampled time at 200ms intervals
-d = np.array([1/2**i for i in np.arange(1, 20, 0.01)])
-N = 100000000
+d = np.array([1/2**i for i in np.arange(1, 20, 0.1)])
+N = 100 * 8 * 1024 * 1024
 
 def P1F(w,d):
     return d ** (w-1)
@@ -21,11 +21,7 @@ def PN0F(w,d):
 def PL(w,d):
     return 1 - P0F(w,d) - P1F(w,d)
 
-
-w = 32
-
-lessList = [k for k in range(1,math.ceil((math.log((math.floor(N/(w-1))),2))/(w-2)) - 1)]
-lessEqualList = [k for k in range(1,math.floor((math.log((math.floor(N/(w-1))),2))/(w-2)))]
+'''
 plt.plot(d, (
     3
     + (math.floor(N / (w-1) - 1)) * (PL(w,d) + PN0F(w,d) * P0F(w,d) + PN1F(w,d) * P1F(w,d))
@@ -33,52 +29,71 @@ plt.plot(d, (
     + sum([
         (math.floor(N/(w-1)) - 2**(k*(w-2))) * (PN0F(w,d) * P0F(w,d)**(2**(k*(w-2))) + PN1F(w,d) * P1F(w,d)**(2**(k*(w-2))))
         for k in lessList])
-    ) * w / N, 'y')
+    ) * w / 8 / 1024 / 1024, 'y')
+'''
 
 
-w=16
+w = 4
 
 lessList = [k for k in range(1,math.ceil((math.log((math.floor(N/(w-1))),2))/(w-2)) - 1)]
 lessEqualList = [k for k in range(1,math.floor((math.log((math.floor(N/(w-1))),2))/(w-2)))]
-plt.plot(d, (
+datapoints = zip(d,(
     3
     + (math.floor(N / (w-1) - 1)) * (PL(w,d) + PN0F(w,d) * P0F(w,d) + PN1F(w,d) * P1F(w,d))
     + sum([(P0F(w,d)**(2**(k*(w-2))) + P1F(w,d)**(2**(k*(w-2)))) for k in lessEqualList])
     + sum([
         (math.floor(N/(w-1)) - 2**(k*(w-2))) * (PN0F(w,d) * P0F(w,d)**(2**(k*(w-2))) + PN1F(w,d) * P1F(w,d)**(2**(k*(w-2))))
         for k in lessList])
-    ) * w / N, 'g')
+    ) * w / N * 100)
+print('\\addplot coordinates { %s };\n\\addlegendentry{\\(w=4\\)};\n' % ' '.join(map(str, datapoints)))
 
 
 w=8
 
 lessList = [k for k in range(1,math.ceil((math.log((math.floor(N/(w-1))),2))/(w-2)) - 1)]
 lessEqualList = [k for k in range(1,math.floor((math.log((math.floor(N/(w-1))),2))/(w-2)))]
-plt.plot(d, (
+datapoints = zip(d,(
     3
     + (math.floor(N / (w-1) - 1)) * (PL(w,d) + PN0F(w,d) * P0F(w,d) + PN1F(w,d) * P1F(w,d))
     + sum([(P0F(w,d)**(2**(k*(w-2))) + P1F(w,d)**(2**(k*(w-2)))) for k in lessEqualList])
     + sum([
         (math.floor(N/(w-1)) - 2**(k*(w-2))) * (PN0F(w,d) * P0F(w,d)**(2**(k*(w-2))) + PN1F(w,d) * P1F(w,d)**(2**(k*(w-2))))
         for k in lessList])
-    ) * w / N, 'b')
+    ) * w / N * 100)
+print('\\addplot coordinates { %s };\n\\addlegendentry{\\(w=8\\)};\n' % ' '.join(map(str, datapoints)))
 
-w=4
+
+w=16
 
 lessList = [k for k in range(1,math.ceil((math.log((math.floor(N/(w-1))),2))/(w-2)) - 1)]
 lessEqualList = [k for k in range(1,math.floor((math.log((math.floor(N/(w-1))),2))/(w-2)))]
-plt.plot(d, (
+datapoints = zip(d,(
     3
     + (math.floor(N / (w-1) - 1)) * (PL(w,d) + PN0F(w,d) * P0F(w,d) + PN1F(w,d) * P1F(w,d))
     + sum([(P0F(w,d)**(2**(k*(w-2))) + P1F(w,d)**(2**(k*(w-2)))) for k in lessEqualList])
     + sum([
         (math.floor(N/(w-1)) - 2**(k*(w-2))) * (PN0F(w,d) * P0F(w,d)**(2**(k*(w-2))) + PN1F(w,d) * P1F(w,d)**(2**(k*(w-2))))
         for k in lessList])
-    ) * w / N, 'r')
+    ) * w / N * 100)
+print('\\addplot coordinates { %s };\n\\addlegendentry{\\(w=16\\)};\n' % ' '.join(map(str, datapoints)))
 
 
-plt.plot(d, 1+d-d, 'k--')
-plt.xscale('log')
-plt.yscale('log')
+w=32
+
+lessList = [k for k in range(1,math.ceil((math.log((math.floor(N/(w-1))),2))/(w-2)) - 1)]
+lessEqualList = [k for k in range(1,math.floor((math.log((math.floor(N/(w-1))),2))/(w-2)))]
+datapoints = zip(d,(
+    3
+    + (math.floor(N / (w-1) - 1)) * (PL(w,d) + PN0F(w,d) * P0F(w,d) + PN1F(w,d) * P1F(w,d))
+    + sum([(P0F(w,d)**(2**(k*(w-2))) + P1F(w,d)**(2**(k*(w-2)))) for k in lessEqualList])
+    + sum([
+        (math.floor(N/(w-1)) - 2**(k*(w-2))) * (PN0F(w,d) * P0F(w,d)**(2**(k*(w-2))) + PN1F(w,d) * P1F(w,d)**(2**(k*(w-2))))
+        for k in lessList])
+    ) * w / N * 100)
+print('\\addplot coordinates { %s };\n\\addlegendentry{\\(w=32\\)};\n' % ' '.join(map(str, datapoints)))
+
+
+# plt.plot(d, 1+d-d, 'k--')
+# plt.xscale('log')
 # plt.xscale('logit')
-plt.show()
+# plt.show()
