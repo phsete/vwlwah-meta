620a5a90551e32dcc4eba319e377749500ff3f1b
Florian Grieskamp <f.grieskamp@gmail.com>
2019-06-06 19:08:40 +0200

    Implement emit functions for compressed words

--- vhdl/vwl_encoder/encoder.vhdl
+++ vhdl/vwl_encoder/encoder.vhdl
@@ -7,16 +7,34 @@ entity encoder is
     -- blk_in: next block of size w-1
     -- blk_out: next block of size w-1
     port (
-        clk:        in std_logic;
-        blk_in:     in std_logic_vector(3 downto 0);
-        blk_out:    out std_logic_vector(4 downto 0)
-    );
+             clk:        in std_logic;
+             blk_in:     in std_logic_vector(3 downto 0);
+             blk_out:    out std_logic_vector(4 downto 0)
+         );
 
-    signal zero_fill_length : natural := 1; -- fill_length is always 1 ahead
-    signal one_fill_length : natural := 1; -- fill_length is always 1 ahead
+         signal zero_fill_length : natural := 1; -- fill_length is always 1 ahead
+         signal one_fill_length : natural := 1; -- fill_length is always 1 ahead
 end encoder;
 
 architecture IMP of encoder is
+
+    function emit_literal (content: std_logic_vector(3 downto 0)) return std_logic_vector is
+        variable output: std_logic_vector(4 downto 0);
+    begin
+        output(4) := '0';
+        output(3 downto 0) := content;
+        return output;
+    end function emit_literal;
+
+    function emit_fill (fill_type: std_logic; length: natural) return std_logic_vector is
+        variable output: std_logic_vector(4 downto 0);
+    begin
+        output(4) := '1';
+        output(3) := fill_type;
+        output(2 downto 0) := std_logic_vector(to_unsigned(length, 3));
+        return output;
+    end function emit_fill;
+
 begin
     process (clk)
     begin
@@ -24,24 +42,19 @@ begin
             if blk_in = "0000" then
                 -- input is zero fill
                 one_fill_length <= 1; -- set back one fill length
-                blk_out(4) <= '1';
-                blk_out(3) <= '0';
-                blk_out(2 downto 0) <= std_logic_vector(to_unsigned(zero_fill_length, 3));
+                blk_out <= emit_fill ('0', zero_fill_length);
                 zero_fill_length <= zero_fill_length + 1;
             elsif blk_in = "1111" then
                 -- input is one fill
                 zero_fill_length <= 1; -- set back zero fill length
-                blk_out(4) <= '1';
-                blk_out(3) <= '1';
-                blk_out(2 downto 0) <= std_logic_vector(to_unsigned(one_fill_length, 3));
+                blk_out <= emit_fill ('1', one_fill_length);
                 one_fill_length <= one_fill_length + 1;
             else
                 -- input is literal word
                 zero_fill_length <= 1;
                 one_fill_length <= 1;
-                blk_out(4) <= '0';
-                blk_out(3 downto 0) <= blk_in;
+                blk_out <= emit_literal(blk_in);
             end if;
         end if;
-        end process;
+    end process;
 end IMP;
