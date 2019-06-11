3e4752501247663aabf91b81ca0f432382c62a88
Florian Grieskamp <f.grieskamp@gmail.com>
2019-06-11 09:23:08 +0200

    Implement emission of multiple output words

--- vhdl/vwl_encoder/encoder.vhdl
+++ vhdl/vwl_encoder/encoder.vhdl
@@ -9,11 +9,15 @@ entity encoder is
     port (
              clk:        in std_logic;
              blk_in:     in std_logic_vector(3 downto 0);
-             blk_out:    out std_logic_vector(4 downto 0)
+             blk_out_0:  out std_logic_vector(4 downto 0);
+             blk_out_1:  out std_logic_vector(4 downto 0);
+             blk_out_2:  out std_logic_vector(4 downto 0);
+             blk_out_3:  out std_logic_vector(4 downto 0)
          );
 
-         signal zero_fill_length : natural := 1; -- fill_length is always 1 ahead
-         signal one_fill_length : natural := 1; -- fill_length is always 1 ahead
+         signal zero_fill_length : unsigned (31 downto 0) := to_unsigned(1, 32); -- fill_length is always 1 ahead
+         signal one_fill_length : unsigned (31 downto 0) := to_unsigned(1, 32); -- fill_length is always 1 ahead
+        
 end encoder;
 
 architecture IMP of encoder is
@@ -26,12 +30,20 @@ architecture IMP of encoder is
         return output;
     end function emit_literal;
 
-    function emit_fill (fill_type: std_logic; length: natural) return std_logic_vector is
+    function emit_fill (fill_type: std_logic; length: unsigned; word_no: natural) return std_logic_vector is
         variable output: std_logic_vector(4 downto 0);
+        variable length_vector: std_logic_vector(31 downto 0);
+        variable lowest_bit_idx: natural;
     begin
-        output(4) := '1';
-        output(3) := fill_type;
-        output(2 downto 0) := std_logic_vector(to_unsigned(length, 3));
+        length_vector := std_logic_vector(length);
+        lowest_bit_idx := word_no * 3;
+        if length_vector(31 downto lowest_bit_idx) /= (31 downto lowest_bit_idx => '0') then
+            output(4) := '1';
+            output(3) := fill_type;
+            output(2 downto 0) := length_vector(lowest_bit_idx + 2 downto lowest_bit_idx);
+        else
+            output := (others => '0');
+        end if;
         return output;
     end function emit_fill;
 
@@ -41,19 +53,25 @@ begin
         if clk'event and clk='1' then
             if blk_in = "0000" then
                 -- input is zero fill
-                one_fill_length <= 1; -- set back one fill length
-                blk_out <= emit_fill ('0', zero_fill_length);
+                one_fill_length <= to_unsigned(1, 32); -- set back one fill length
+                blk_out_0 <= emit_fill ('0', zero_fill_length, 0);
+                blk_out_1 <= emit_fill ('0', zero_fill_length, 1);
+                blk_out_2 <= emit_fill ('0', zero_fill_length, 2);
+                blk_out_3 <= emit_fill ('0', zero_fill_length, 3);
                 zero_fill_length <= zero_fill_length + 1;
             elsif blk_in = "1111" then
                 -- input is one fill
-                zero_fill_length <= 1; -- set back zero fill length
-                blk_out <= emit_fill ('1', one_fill_length);
+                zero_fill_length <= to_unsigned(1, 32); -- set back zero fill length
+                blk_out_0 <= emit_fill ('1', one_fill_length, 0);
+                blk_out_1 <= emit_fill ('1', one_fill_length, 1);
+                blk_out_2 <= emit_fill ('1', one_fill_length, 2);
+                blk_out_3 <= emit_fill ('1', one_fill_length, 3);
                 one_fill_length <= one_fill_length + 1;
             else
                 -- input is literal word
-                zero_fill_length <= 1;
-                one_fill_length <= 1;
-                blk_out <= emit_literal(blk_in);
+                zero_fill_length <= to_unsigned(1, 32);
+                one_fill_length <= to_unsigned(1, 32);
+                blk_out_0 <= emit_literal(blk_in);
             end if;
         end if;
     end process;
--- vhdl/vwl_encoder/encoder_tb.vhdl
+++ vhdl/vwl_encoder/encoder_tb.vhdl
@@ -7,25 +7,49 @@ entity encoder_tb is
     end encoder_tb;
 
 architecture behav of encoder_tb is
+
+    -- found this function implementation at: https://stackoverflow.com/questions/15406887/vhdl-convert-vector-to-string
+    function to_string ( a: std_logic_vector) return string is
+        variable b : string (1 to a'length) := (others => NUL);
+        variable stri : integer := 1; 
+    begin
+        for i in a'range loop
+            b(stri) := std_logic'image(a((i)))(2);
+            stri := stri+1;
+        end loop;
+        return b;
+    end function;
+
     --  Declaration of the component that will be instantiated.
     component encoder
         port (
                  clk:        in std_logic;
                  blk_in:     in std_logic_vector(3 downto 0);
-                 blk_out:    out std_logic_vector(4 downto 0)
+                 blk_out_0:  out std_logic_vector(4 downto 0);
+                 blk_out_1:  out std_logic_vector(4 downto 0);
+                 blk_out_2:  out std_logic_vector(4 downto 0);
+                 blk_out_3:  out std_logic_vector(4 downto 0)
              );
     end component;
 
     --  Specifies which entity is bound with the component.
     for encoder_0: encoder use entity work.encoder;
-    signal clk:     std_logic;
-    signal blk_in:  std_logic_vector(3 downto 0);
-    signal blk_out: std_logic_vector(4 downto 0);
+    signal clk:         std_logic;
+    signal blk_in:      std_logic_vector(3 downto 0);
+    signal blk_out_0:   std_logic_vector(4 downto 0);
+    signal blk_out_1:   std_logic_vector(4 downto 0);
+    signal blk_out_2:   std_logic_vector(4 downto 0);
+    signal blk_out_3:   std_logic_vector(4 downto 0);
+
     begin
         --  Component instantiation.
         encoder_0: encoder
             -- THIS IS HOW IT WORKS WITH GENERICS: generic map (WORD_SIZE <= 5)
-            port map (clk => clk, blk_in => blk_in, blk_out => blk_out);
+            port map (clk => clk, blk_in => blk_in,
+                      blk_out_0 => blk_out_0,
+                      blk_out_1 => blk_out_1,
+                      blk_out_2 => blk_out_2,
+                      blk_out_3 => blk_out_3);
 
         --  This process does the real job.
         process
@@ -33,32 +57,42 @@ architecture behav of encoder_tb is
             --  The inputs of the encoder.
             blk_in: std_logic_vector(3 downto 0);
             --  The expected outputs of the encoder.
-            blk_out: std_logic_vector(4 downto 0);
+            blk_out_0: std_logic_vector(4 downto 0);
+            blk_out_1: std_logic_vector(4 downto 0);
+            blk_out_2: std_logic_vector(4 downto 0);
+            blk_out_3: std_logic_vector(4 downto 0);
         end record;
         --  The patterns to apply.
         type pattern_array is array (natural range <>) of pattern_type;
         constant patterns : pattern_array :=
-        (("0000", "10001"),
-        ("0000", "10010"),
-        ("0000", "10011"),
-        ("1111", "11001"),
-        ("1111", "11010"),
-        ("1111", "11011"),
-        ("0001", "00001"),
-        ("0010", "00010"),
-        ("0100", "00100"),
-        ("1111", "11001"),
-        ("1111", "11010"),
-        ("1111", "11011"),
-        ("0000", "10001"),
-        ("0000", "10010"),
-        ("0000", "10011"),
-        ("0001", "00001"),
-        ("0010", "00010"),
-        ("0100", "00100"),
-        ("0000", "10001"),
-        ("0000", "10010"),
-        ("0000", "10011"));
+        (("0000", "10001", "00000", "00000", "00000"),
+        ("0000", "10010", "00000", "00000", "00000"),
+        ("0000", "10011", "00000", "00000", "00000"),
+        ("0000", "10100", "00000", "00000", "00000"),
+        ("0000", "10101", "00000", "00000", "00000"),
+        ("0000", "10110", "00000", "00000", "00000"),
+        ("0000", "10111", "00000", "00000", "00000"),
+        ("0000", "10000", "10001", "00000", "00000"),
+        ("0000", "10001", "10001", "00000", "00000"),
+        ("0000", "10010", "10001", "00000", "00000"),
+        ("1111", "11001", "00000", "00000", "00000"),
+        ("1111", "11010", "00000", "00000", "00000"),
+        ("1111", "11011", "00000", "00000", "00000"),
+        ("0001", "00001", "00000", "00000", "00000"),
+        ("0010", "00010", "00000", "00000", "00000"),
+        ("0100", "00100", "00000", "00000", "00000"),
+        ("1111", "11001", "00000", "00000", "00000"),
+        ("1111", "11010", "00000", "00000", "00000"),
+        ("1111", "11011", "00000", "00000", "00000"),
+        ("0000", "10001", "00000", "00000", "00000"),
+        ("0000", "10010", "00000", "00000", "00000"),
+        ("0000", "10011", "00000", "00000", "00000"),
+        ("0001", "00001", "00000", "00000", "00000"),
+        ("0010", "00010", "00000", "00000", "00000"),
+        ("0100", "00100", "00000", "00000", "00000"),
+        ("0000", "10001", "00000", "00000", "00000"),
+        ("0000", "10010", "00000", "00000", "00000"),
+        ("0000", "10011", "00000", "00000", "00000"));
         begin
             assert false report "begin of test" severity note;
 
@@ -76,8 +110,14 @@ architecture behav of encoder_tb is
                 wait for 1 ns;
 
                 --  Check the outputs.
-                assert blk_out = patterns(i).blk_out
-                report "bad encoding in output word" severity error;
+                assert blk_out_0 = patterns(i).blk_out_0
+                report "bad encoding in output word 0. Expected: " & to_string(patterns(i).blk_out_0) & " but found " & to_string(blk_out_0) severity error;
+                assert blk_out_1 = patterns(i).blk_out_1
+                report "bad encoding in output word 1. Expected: " & to_string(patterns(i).blk_out_1) & " but found " & to_string(blk_out_1) severity error;
+                assert blk_out_2 = patterns(i).blk_out_2
+                report "bad encoding in output word 2. Expected: " & to_string(patterns(i).blk_out_2) & " but found " & to_string(blk_out_2) severity error;
+                assert blk_out_3 = patterns(i).blk_out_3
+                report "bad encoding in output word 3. Expected: " & to_string(patterns(i).blk_out_3) & " but found " & to_string(blk_out_3) severity error;
             end loop;
 
             assert false report "end of test" severity note;
