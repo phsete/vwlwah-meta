8b7d2b7635d1c405a15b51f373be58eed5a7aea0
Florian Grieskamp <f.grieskamp@gmail.com>
2019-06-18 10:22:36 +0200

    Implement inverter

---
+++ vhdl/vwlwah/src/inverter.vhdl
@@ -0,0 +1,117 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.math_real.log2;
+use ieee.math_real.ceil;
+
+entity inverter is
+    Generic (
+                word_size:           natural := 5;
+                fill_counter_size:   natural := 32
+            );
+    port (
+             clk:           in std_logic;
+             blk_in:        in std_logic_vector(word_size-1 downto 0);
+             in_empty:      in std_logic;
+             out_full:      in std_logic;
+             blk_out:       out std_logic_vector(word_size-1 downto 0);
+             in_rd:         out std_logic;
+             out_wr:        out std_logic
+         );
+
+end inverter;
+
+architecture IMP of inverter is
+
+    type Word is (W_NONE, W_0FILL, W_1FILL, W_LITERAL);
+
+    signal current_word:        std_logic_vector(word_size-1 downto 0);
+    signal output_buffer:       std_logic_vector(word_size-1 downto 0);
+    signal input_available:     std_logic := '0';
+    signal in_rd_loc:           std_logic;
+    signal out_wr_loc:          std_logic;
+    signal running:             std_logic := '1';
+    signal current_type:        Word := W_NONE;
+
+begin
+    process (clk)
+
+        -- writes a literal word to the output buffer
+
+        function parse_word_type (input_word: std_logic_vector(word_size-1 downto 0)) return Word is
+        begin
+            if input_word(word_size-1) = '0' then
+                return W_LITERAL;
+            elsif input_word(word_size-2) = '0' then
+                return W_0FILL;
+            elsif input_word(word_size-2) = '1' then
+                return W_1FILL;
+            else
+                return W_NONE;
+            end if;
+        end parse_word_type;
+
+        function convert_F (input: std_logic_vector(word_size-1 downto 0)) return std_logic_vector is
+            variable output: std_logic_vector(word_size-1 downto 0) := (others => 'U');
+        begin
+            output(word_size-1) := input(word_size-1);
+            output(word_size-2) := not(input(word_size-2));
+            output(word_size-3 downto 0) := input(word_size-3 downto 0);
+            return output;
+        end convert_F;
+
+        function convert_L (input: std_logic_vector(word_size-1 downto 0)) return std_logic_vector is
+            variable output: std_logic_vector(word_size-1 downto 0);
+        begin
+            output(word_size-1) := input(word_size-1);
+            for i in word_size-2 downto 0 loop
+                output(i) := not(input(i));
+            end loop;
+            return output;
+        end convert_L;
+
+    begin
+        if (clk'event and clk='1' and running = '1') then
+            case current_type is
+                when W_0FILL | W_1FILL =>
+                    output_buffer <= convert_F(current_word);
+                    out_wr_loc <= '1';
+                when W_LITERAL =>
+                    output_buffer <= convert_L(current_word);
+                    out_wr_loc <= '1';
+                when others =>
+                    out_wr_loc <= '0';
+            end case;
+
+            input_available <= not(in_empty);
+        end if;
+
+        if (clk'event and clk='0') then
+            -- read the next word and push buffers forward
+            if (input_available = '1') then
+                current_word <= blk_in;
+                current_type <= parse_word_type(blk_in);
+            else
+                current_word <= (others => 'U');
+                current_type <= W_NONE;
+            end if;
+
+            -- ready to write output value
+            if (out_wr_loc = '1' and out_full = '0') then
+                blk_out <= output_buffer;
+            end if;
+
+            -- stop processing if output buffer is full
+            if (out_full = '0') then
+                running <= '1';
+            else
+                running <= '0';
+            end if;
+        end if;
+
+    end process;
+
+    in_rd  <= '1';
+    out_wr <= out_wr_loc;
+
+end IMP;
---
+++ vhdl/vwlwah/src/inverter_tb.vhdl
@@ -0,0 +1,192 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+--  A testbench has no ports.
+entity inverter_tb is
+    end inverter_tb;
+
+architecture behav of inverter_tb is
+
+    -- found this function implementation at: https://stackoverflow.com/questions/15406887/vhdl-convert-vector-to-string
+    function to_string ( a: std_logic_vector) return string is
+        variable b : string (1 to a'length) := (others => NUL);
+        variable stri : integer := 1; 
+    begin
+        for i in a'range loop
+            b(stri) := std_logic'image(a((i)))(2);
+            stri := stri+1;
+        end loop;
+        return b;
+    end function;
+
+    -- Declaration of the components that will be instantiated.
+    component inverter is
+        Generic (
+                    word_size:           natural := 5;
+                    fill_counter_size:   natural := 32
+                );
+        port (
+                 clk:           in std_logic;
+                 blk_in:        in std_logic_vector(word_size-1 downto 0);
+                 in_empty:      in std_logic;
+                 out_full:      in std_logic;
+                 blk_out:       out std_logic_vector(word_size-1 downto 0);
+                 in_rd:         out std_logic;
+                 out_wr:        out std_logic
+             );
+    end component;
+
+    component input_fifo
+        Generic (
+                    constant Addrbreite: natural := 3;
+                    constant Wortbreite: natural := 5
+                );
+        Port ( Din   : in  STD_LOGIC_VECTOR (Wortbreite-1 downto 0);
+               Wr    : in  STD_LOGIC;
+               Dout  : out STD_LOGIC_VECTOR (Wortbreite-1 downto 0);
+               Rd    : in  STD_LOGIC;
+               Empty : out STD_LOGIC;
+               Full  : out STD_LOGIC;
+               CLK   : in  STD_LOGIC
+           );
+    end component;
+
+    component output_fifo
+        Generic (
+                    constant Addrbreite: natural := 3;
+                    constant Wortbreite: natural := 5
+                );
+        Port ( Din   : in  STD_LOGIC_VECTOR (Wortbreite-1 downto 0);
+               Wr    : in  STD_LOGIC;
+               Dout  : out STD_LOGIC_VECTOR (Wortbreite-1 downto 0);
+               Rd    : in  STD_LOGIC;
+               Empty : out STD_LOGIC;
+               Full  : out STD_LOGIC;
+               CLK   : in  STD_LOGIC
+           );
+    end component;
+
+    --  Specifies which entity is bound with the component.
+    for inverter_0: inverter use entity work.inverter;
+    for input_fifo_0: input_fifo use entity work.FIFO;
+    for output_fifo_0: output_fifo use entity work.FIFO;
+
+    -- inner signals
+    signal blk_in:          std_logic_vector(4 downto 0);
+    signal blk_out:         std_logic_vector(4 downto 0);
+    signal in_empty:        std_logic;
+    signal out_full:        std_logic;
+    signal in_rd:           std_logic;
+    signal out_wr:          std_logic;
+
+    -- outer signals
+    signal outer_clk:      std_logic;
+    signal outer_input:    std_logic_vector(4 downto 0);
+    signal outer_wr_en:    std_logic;
+    signal outer_rd_en:    std_logic;
+    signal outer_output:   std_logic_vector(4 downto 0);
+    signal outer_empty:    std_logic;
+    signal outer_full:     std_logic;
+
+    begin
+        --  Component instantiation.
+        inverter_0: inverter
+        port map (clk => outer_clk,
+                  blk_in => blk_in,
+                  blk_out => blk_out,
+                  in_empty => in_empty,
+                  out_full => out_full,
+                  in_rd => in_rd,
+                  out_wr => out_wr);
+
+        input_fifo_0: input_fifo
+        port map (CLK => outer_clk,
+                  Din => outer_input,
+                  Wr => outer_wr_en,
+                  Dout => blk_in,
+                  Rd => in_rd,
+                  Empty => in_empty,
+                  Full => outer_full);
+
+        output_fifo_0: output_fifo
+        port map (CLK => outer_clk,
+                  Din => blk_out,
+                  Wr => out_wr,
+                  Dout => outer_output,
+                  Rd => outer_rd_en,
+                  Empty => outer_empty,
+                  Full => out_full);
+
+        --  This process does the real job.
+        process
+        type pattern_type is record
+            --  The inputs of the inverter.
+            outer_input: std_logic_vector(4 downto 0);
+            outer_wr_en: std_logic;
+            outer_rd_en: std_logic;
+            --  The expected outputs of the inverter.
+            outer_output: std_logic_vector(4 downto 0);
+            outer_empty: std_logic;
+        end record;
+        --  The patterns to apply.
+        type pattern_array is array (natural range <>) of pattern_type;
+        constant patterns : pattern_array :=
+        (("00001", '1', '1', "UUUUU", '1'),  -- 00
+         ("10011", '1', '1', "UUUUU", '1'),  -- 01
+         ("11001", '1', '1', "UUUUU", '1'),  -- 02
+         ("11010", '1', '1', "UUUUU", '0'),  -- 03
+         ("00001", '1', '1', "01110", '0'),  -- 04
+         ("00010", '1', '1', "11011", '0'),  -- 05
+         ("00100", '0', '1', "10001", '0'),  -- 06
+         ("01000", '0', '1', "10010", '0'),  -- 07
+         ("00001", '0', '1', "01110", '0'),  -- 08
+         ("00000", '0', '1', "01101", '1'),  -- 09
+         ("00000", '0', '1', "01101", '1'),  -- 10
+         ("00000", '0', '1', "01101", '1'),  -- 11
+         ("00000", '0', '1', "01101", '1'),  -- 12
+         ("00000", '0', '1', "01101", '1'),  -- 13
+         ("00000", '0', '1', "01101", '1'),  -- 14
+         ("00000", '0', '1', "01101", '1'),  -- 15
+         ("00000", '0', '1', "01101", '1'),  -- 16
+         ("00000", '0', '1', "01101", '1'),  -- 17
+         ("00000", '0', '1', "01101", '1'),  -- 18
+         ("00000", '0', '1', "01101", '1'),  -- 19
+         ("00000", '0', '1', "01101", '1'),  -- 20
+         ("00000", '0', '1', "01101", '1'),  -- 21
+         ("00000", '0', '1', "01101", '1'),  -- 21
+         ("00000", '0', '1', "01101", '1'),  -- 21
+         ("00000", '0', '1', "01101", '1'),  -- 21
+         ("00000", '0', '1', "01101", '1'),  -- 21
+         ("00000", '0', '1', "01101", '1')); -- 21
+        begin
+            assert false report "begin of test" severity note;
+
+            --  Check each pattern.
+            for i in patterns'range loop
+                --  Set the inputs.
+                outer_wr_en <= patterns(i).outer_wr_en;
+                outer_input <= patterns(i).outer_input;
+                outer_rd_en <= patterns(i).outer_rd_en;
+
+                -- simulate the clock
+                outer_clk <= '0';
+                wait for 1 ns;
+                outer_clk <= '1';
+
+                --  Wait for the results.
+                wait for 1 ns;
+
+                --  Check the outputs.
+                assert outer_empty = patterns(i).outer_empty
+                report "empty state incorrect in test " & integer'image(i) & ". Expected: " & std_logic'image(patterns(i).outer_empty) & " but found " & std_logic'image(outer_empty) severity error;
+
+                assert outer_output = patterns(i).outer_output
+                report "bad decoding in test " & integer'image(i) & ". Expected: " & to_string(patterns(i).outer_output) & " but found " & to_string(outer_output) severity error;
+            end loop;
+
+            assert false report "end of test" severity note;
+            --  Wait forever; this will finish the simulation.
+            wait;
+        end process;
+    end behav;
