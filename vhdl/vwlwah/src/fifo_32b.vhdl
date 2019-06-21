e391a1bfa7bae25eb86dfd0475c0edbdd5682a3f
Florian Grieskamp <f.grieskamp@gmail.com>
2019-06-21 12:10:02 +0200

    Implement reset and final feature for fifo_32b

--- vhdl/vwlwah/src/fifo_32b.vhdl
+++ vhdl/vwlwah/src/fifo_32b.vhdl
@@ -8,13 +8,16 @@ entity FIFO_32B is
                 Addrbreite  : natural := 8;  -- Speicherlänge = 2^Addrbreite
                 Wortbreite  : natural := 8
             );
-    Port ( Din   : in  STD_LOGIC_VECTOR (31 downto 0);
-           Wr    : in  STD_LOGIC;
-           Dout  : out STD_LOGIC_VECTOR (Wortbreite-1 downto 0);
-           Rd    : in  STD_LOGIC;
-           Empty : out STD_LOGIC;
-           Full  : out STD_LOGIC;
-           CLK   : in  STD_LOGIC
+    Port ( Din:         in  STD_LOGIC_VECTOR (31 downto 0);
+           Wr:          in  STD_LOGIC;
+           Dout:        out STD_LOGIC_VECTOR (Wortbreite-1 downto 0);
+           Rd:          in  STD_LOGIC;
+           Empty:       out STD_LOGIC;
+           Full:        out STD_LOGIC;
+           Final_in:    in  STD_LOGIC;
+           Final_out:   out STD_LOGIC;
+           CLK:         in  STD_LOGIC;
+           RESET:       in  STD_LOGIC
        );
 end FIFO_32B;
 
@@ -23,10 +26,13 @@ architecture Verhalten of FIFO_32B is
     signal wrcnt : unsigned (Addrbreite-1 downto 0) := (others => '0');
     signal rdcnt : unsigned (Addrbreite-1 downto 0) := (others => '0');
     signal rdpos : unsigned (4 downto 0) := (others => '1'); -- next unread bit index
+    signal final_idx: unsigned (Addrbreite-1 downto 0) := (others => '0');
+    signal final : boolean := false;
     type speicher is array(0 to (2**Addrbreite)-1) of unsigned(31 downto 0);
     signal memory : speicher;   
     signal full_loc  : std_logic;
     signal empty_loc : std_logic;
+    signal final_out_loc:  std_logic;
 
 begin
     process (CLK)
@@ -61,6 +67,14 @@ begin
             return out_word;
         end read_next;
 
+        impure function is_final return boolean is
+            variable result : boolean;
+        begin
+            result := final and rdcnt = final_idx;
+            result := result and rdpos <= Wortbreite-1;
+            return result;
+        end is_final;
+
     begin
         assert Wortbreite <= 32
         report "FIFO_32b: b must not be greater than 32 (is " & natural'image(Wortbreite) & " )"  severity error;
@@ -68,14 +82,27 @@ begin
         if (clk'event and clk='1') then
             if (Wr='1' and full_loc='0') then
                 write_next(Din);
+                if (Final_in = '1' and not(final)) then
+                    final <= true;
+                    final_idx <= wrcnt;
+                end if;
             end if;
-            if (Rd='1' and empty_loc='0') then
+            if (Rd='1' and empty_loc='0' and final_out_loc='0') then
                 Dout <= read_next;
+            end if;
+            if (RESET = '1') then
+                wrcnt <= (others => '0');
+                rdcnt <= (others => '0');
+                rdpos <= (others => '1'); -- next unread bit index
+                final_idx <= (others => '0');
+                final <= false;
             end if;
         end if;
     end process;
-full_loc  <= '1' when rdcnt = wrcnt+1 else '0';
-empty_loc <= '1' when rdcnt = wrcnt or (rdcnt+1 = wrcnt and rdpos < Wortbreite-1)  else '0';
-Full  <= full_loc;
-Empty <= empty_loc;
+    final_out_loc <= '1' when (final and ((rdcnt=final_idx and rdpos < Wortbreite-1) or rdcnt=final_idx+1)) else '0';
+    full_loc  <= '1' when rdcnt = wrcnt+1 else '0';
+    empty_loc <= '1' when rdcnt = wrcnt or (rdcnt+1 = wrcnt and rdpos < Wortbreite-1)  else '0';
+    Final_out <= final_out_loc;
+    Full  <= full_loc;
+    Empty <= empty_loc;
 end Verhalten;
